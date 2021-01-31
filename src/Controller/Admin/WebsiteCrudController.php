3c94c211eeaaf66920ca127af00d0fec12224dec
guiguibe44 <guillaume.bex44@gmail.com>
2021-01-31 20:44:29 +0100

    first commit

--- src/Controller/Admin/WebsiteCrudController.php
+++ src/Controller/Admin/WebsiteCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Website;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -13,21 +15,32 @@ class WebsiteCrudController extends AbstractCrudController
     {
         return Website::class;
     }
-
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Site web')
+            ->setEntityLabelInPlural('Sites web')
+            ->setPaginatorPageSize(100);
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('domain'),
-            TextField::new('ftpHost')->hideOnIndex(),
-            TextField::new('ftpUser')->hideOnIndex(),
-            TextField::new('CryptedFtpPassword')->hideOnIndex(),
-            TextField::new('sqlAddress')->hideOnIndex(),
-            TextField::new('sqlDataBaseName')->hideOnIndex(),
-            TextField::new('sqlUser')->hideOnIndex(),
-            TextField::new('CryptedSqlPassword')->hideOnIndex(),
-            AssociationField::new('customer'),
+            TextField::new('domain','Domaine'),
+            TextField::new('ftpHost','Hôte FTP')->hideOnIndex(),
+            TextField::new('ftpUser','Utilisateur FTP')->hideOnIndex(),
+            TextField::new('CryptedFtpPassword','Mot de passe FTP')->hideOnIndex(),
+            TextField::new('sqlAddress', 'Adresse SQL')->hideOnIndex(),
+            TextField::new('sqlDataBaseName','Nom de la base SQL')->hideOnIndex(),
+            TextField::new('sqlUser', 'Utilisateur SQL')->hideOnIndex(),
+            TextField::new('CryptedSqlPassword','Mot de passe SQL')->hideOnIndex(),
+            AssociationField::new('customer', 'Client'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, 'detail');
     }
 
 }
